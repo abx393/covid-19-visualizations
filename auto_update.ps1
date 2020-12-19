b17c0c2416ce441e60efb8e84aacf02129fa4e18
abx393 <abandari99@gmail.com>
2020-12-19 13:12:25 -0800

    testing auto update

---
+++ auto_update.ps1
@@ -0,0 +1,7 @@
+﻿wget -O raw_data/time_series_covid19_confirmed_US.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv
+wget -O raw_data/time_series_covid19_confirmed_global.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv
+wget -O raw_data/time_series_covid19_deaths_US.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv
+wget -O raw_data/time_series_covid19_deaths_global.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv
+git add .
+git commit -m "testing auto update"
+git push
